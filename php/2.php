8229631f45e2ba1e1e574714da703e64634d3de5
Md Asif Iqbal <phpkolkata@gmail.com>
2023-02-13 08:06:39 +0530

    php

---
+++ php/2.php
@@ -0,0 +1,10 @@
+<?php
+// variable
+
+$x = "10abc";
+$y = 20;
+
+// $x = "abc"; // no error, supports auto type casting feature
+
+$z = $x + $y;
+echo $z;
---
+++ php/3.php
@@ -0,0 +1,25 @@
+4 types of errors
+
+non blocking error for compilation
+1. Notice
+2. Warning
+
+blocking error
+3. Parse Error (syntax error - 95%)
+4. Fatal Error (logical error - 2%)
+
+
+Parse Error examples
+1. ; missing
+
+2. (() - ) missing / extra
+
+3. {{  } - } missing / extra
+
+4. spelling mistakes
+
+5. syntax mistakes
+
+6. $ missing in variables
+
+Homework - PHP OPERATORS 
