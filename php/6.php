9113bde2fd799873ec12a6100c5a9c9987e761d6
Md Asif Iqbal <phpkolkata@gmail.com>
2023-02-25 08:12:37 +0530

    loops

--- php/6.php
+++ php/6.php
@@ -6,7 +6,38 @@
 // 46-65 -      B       2nd
 // 65 above -   A       1st div
 
-$avg = 33;
-$class = 5;
+$avg = 32;
+$class = 8;
 
+
+if($class <= 5){
+    // Grade System
+    if($avg <=32){
+        echo "D";
+    }
+    else if($avg <= 45){
+        echo "C";
+    }
+    else if($avg <= 65){
+        echo "B";
+    }
+    else{
+        echo "A";
+    }
+}
+else{
+    // divission system
+    if($avg <=32){
+    echo "fail";
+    }
+    else if($avg <= 45){
+        echo "3rd";
+    }
+    else if($avg <= 65){
+        echo "2nd";
+    }
+    else{
+        echo "1st";
+    }
+}
 
---
+++ php/7.php
@@ -0,0 +1,62 @@
+<?php
+
+// loops
+
+// for - logical building
+// while - record/row fetching
+// do while - almost never
+
+
+// 3 parameters to run a loop
+// 1. initialization
+// 2. condition
+// 3. increament/decrement
+// -------------------------------------------
+
+// for($i=0;$i<=100;$i++)
+// {
+//     echo $i."<br>";
+// }
+
+
+// $i=0;
+// while($i<=10){
+//     echo $i."<br>";
+//     $i++;
+// }
+
+
+// $i=11;
+// do{
+//    echo $i."<br>";
+//    $i++;
+// }while($i<=10);
+
+
+home work: loop for 1-10
+
+1 - odd
+2 - even
+3
+4
+5
+6
+7
+8
+9
+10
+
+----------------
+
+2
+4
+6
+8
+10
+
+-----------------
+1
+3
+5
+7
+9
